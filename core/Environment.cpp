d53496f73d18ed0a83b1781d6a920e8bd0248eca
Ömer Kayan <81143460+omerkayan@users.noreply.github.com>
2023-01-17 21:12:49 +0100

    Update Environment.cpp

--- core/Environment.cpp
+++ core/Environment.cpp
@@ -662,9 +662,9 @@ GetReward()
 	
 	
 	//kuvvetin yerini gosterme amacli. Egitim sirasinda burasi kapalı olunca hizli oluyor. NN klasoru kaydet. Burayi ac. Calistir. NNleri yerine getir.
-	//disEtkiGosterOrta();
-	//disEtkiGosterUst();
-	//disEtkiGosterAlt();	
+	disEtkiGosterOrta();
+	disEtkiGosterUst();
+	disEtkiGosterAlt();	
 	
 	//std::cout<<"r_ee  "<<r_ee<<std::endl;
 	return r;
